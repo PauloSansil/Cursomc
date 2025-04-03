a18c078b98b60cd40867623eb625e4a9d4db0799
Paulo Santana <sansilsoft@outlook.com>
2025-04-03 18:05:53 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/sansil/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/sansil/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,12 @@
 package com.sansil.cursomc.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sansil.cursomc.domain.Categoria;
+import com.sansil.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 
 }
